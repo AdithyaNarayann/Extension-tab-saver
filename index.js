77ff2f49b27188df302844e515c663b588ea6f78
Adithya Narayan <adithyanarayankannan@gmail.com>
2025-06-08 19:23:56 +0530

    pushed

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-function savebtn(){
-    console.log("button clicked")
+let myleads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+
+inputBtn.addEventListener("click",function() {
+    myleads.push(inputEl.value)
+    inputEl.value = ""
+    inputEl.focus()
+    renderlist()   
+})
+
+function renderlist() {
+    let listitems = ""
+    for(let  i=0;i<myleads.length;i++){
+        // const li = document.createElement("li")
+        // li.textContent = myleads[i]
+        // ulEl.append(li)
+        listitems += `
+            <li>
+                <a href= '${myleads[i]}' target='_blank'>${myleads[i]}</a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listitems
+
 }
